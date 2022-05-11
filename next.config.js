5a031c7fdc086ba44b8978ad9c80125b70d3273b
Milana Birulia <lianabir@mail.ru>
2022-05-11 11:38:54 +0300

    update gitignore

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== 'production'
-
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['strapi.po-stroy31.ru'],
-  },
-}
